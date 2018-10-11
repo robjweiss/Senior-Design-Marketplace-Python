f05c755848515a20e148ccc3724801b96d1ffeb5
khockenj <slyster1010@gmail.com>
2018-10-11 13:19:21 -0400

    initial ui commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
@@ -9,7 +9,26 @@ app.config.from_object('config')
 # Database Object
 db = SQLAlchemy(app)
 
-# temporary route
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	return render_template('index.html')	
+
+@app.route('/admin')
+def professors():
+	return render_template('admin.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/createProject')
+def createProject():
+	return render_template('createProject.html')
+
+@app.route('/view')
+def view():
+	return render_template('view.html')
+
+@app.route('/createProposal')
+def proposal():
+	return render_template('createProposal.html')
