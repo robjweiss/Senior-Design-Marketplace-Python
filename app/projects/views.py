c64b2a561f408451131e41d9640d0b31bcb3cf8f
robjweiss <rweiss55@gmail.com>
2018-10-18 12:33:32 -0400

    Created database code to add projects

--- app/projects/views.py
+++ app/projects/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, request, flash
 
 from . import projects
 from .models import Project
+from .. import db
 
 @projects.route('/', methods=['GET', 'POST'])
 def get_projects():
@@ -14,3 +15,20 @@ def get_projects():
 			dateSwitcher = "at " + row.date.strftime('%I:%M%p')
 		project_list.append([row.id, row.title, row.desc, row.author, dateSwitcher, row.views, row.visibility, row.sponsors, row.locked])
 	return render_template('projects.html', projects=project_list)
+
+@projects.route('/createProject/add', methods=['GET', 'POST'])
+def add_project():
+    project = Project(title = request.form.get(title),
+                        desc = request.form.get(description),
+                        date = request.form.get(lockout_date),
+                        visibility = request.form.get(visibility),
+                        sponsors = request.form.get(sponsors),
+                        locked = request.form.get(locked)
+                        )
+
+    try:
+        db.session.add(project)
+        db.session.commit()
+        flash('Successfully added project')
+    except:
+        flash('An error occurred adding project to the database')
