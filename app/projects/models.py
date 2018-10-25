2edfea640da8dd1ac3c19b7797ee65d5f4b99de5
khockenj <slyster1010@gmail.com>
2018-10-25 14:37:48 -0400

    QR Code

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,12 +1,11 @@
 from app import db
-
 class Project(db.Model):
 
 	__tablename__ = 'projects'
 
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(60), index=True, unique=True)
-	desc = db.Column(db.String(5000))
+	desc = db.Column(db.Text(4294000000))
 	author = db.Column(db.String(50), index=True)
 	date = db.Column(db.DateTime, index=True)
 	views = db.Column(db.Integer, index=True)
@@ -20,7 +19,7 @@ class Proposal(db.Model):
 	
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(60), index=True, unique=True)
-	desc = db.Column(db.String(5000))
+	desc = db.Column(db.Text(4294000000))
 	author = db.Column(db.String(50), index=True)
 	date = db.Column(db.DateTime, index=True)
 	views = db.Column(db.Integer, index=True)
--- app/static/jsObjects.js
+++ app/static/jsObjects.js
@@ -1,25 +1,25 @@
 function projectCard(id, title, desc, date, locked) {
 return `
-<div class="card">
+<div class="card text-center">
 	<div class="card-body">
 		<h5 class="card-title">${locked}${title}</h5>
-		<p class="card-text">${desc}</p>
+		<p class="card-text" id='shortDesc${id}'>${desc}</p>
 		<p class="card-text"><small class="text-muted">Last updated ${date}</small>
 	</div>
-	<div class="card-footer">
-		<a href="#" class="btn btn-primary disabled">Apply</a>
-		<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modal${id}">More Info</a>
+	<div class="card-footer float-right text-right">
+
+		<a href="#" class="btn btn-primary float-right" data-toggle="modal" id='moreInfo' data-target="#modal${id}">More Info</a>
 	</div>
 </div>`;
 }
 
-function projectModal(id, title, desc, date, views, author, locked) {
+function projectModal(id, title, desc, date, views, author, locked, QRurl) {
 	return `
-<div class="modal fade bd-example-modal-lg" id="modal${id}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-lg viewCheck" id="modal${id}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
 	<div class="modal-dialog modal-lg">
 	<div class="modal-content">
 	<div class="modal-header">
-	<h5 class="modal-title" id="exampleModalLabel">${locked}${title} <button type="button" class="close"  id="favButton" style="padding-left:0.2rem !important; color:red !important;"><span id="favText" class="material-icons">favorite_border</span></button></h5>
+	<h5 class="modal-title" id="exampleModalLabel">${locked}${title} <button type="button" class="close"  id="favButton$" style="padding-left:0.2rem !important; color:red !important;"><span id="favText" class="material-icons">favorite_border</span></button></h5>
 	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 	</div>
 		
@@ -34,13 +34,19 @@ function projectModal(id, title, desc, date, views, author, locked) {
 		<li class="nav-item">
 			<a class="nav-link" id="memberTab" data-toggle="tab" href="#member" role="tab" aria-controls="memberTab" aria-selected="false">Current Members</a>
 		</li>
+		<li class="nav-item">
+			<a class="nav-link" id="advisorTab" data-toggle="tab" href="#advisor" role="tab" aria-controls="advisorTab" aria-selected="false">Advisors</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" id="qrTab" data-toggle="tab" href="#qrTest" role="tab" aria-controls="qrTab" aria-selected="false">QR Code</a>
+		</li>
 		</ul>
 	<div class="modal-content-tldr"><span class="infoSpan"><span class="material-icons" style="color:red">whatshot</span> ${views} views</span><span class="infoSpan">Created by: <a class='regularLink'>${author}</a> ${date}</span><span class="infoSpan-last"><span class="material-icons" style="color:red">favorite</span>xxx</span></div>
 	<div class="container-fluid">
 	<div class="tab-content" id="myTabContent">
 	<!--General-->
 	<div class="tab-pane fade show active modal-content-true" id="general" role="tabpanel" aria-labelledby="generalTab">
-		content
+	${desc}
 	</div>
 	<!--Looking For-->
 	<div class="tab-pane fade modal-content-true" id="lookingFor" role="tabpanel" aria-labelledby="lookingTab">
@@ -90,7 +96,7 @@ function projectModal(id, title, desc, date, views, author, locked) {
 			<div class="row">
 			<div class="col-md-4 pictureAlign">
 			<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-			<h5>schad</h5>
+			<h5>chad</h5>
 			</div>
 			<div class="col-md-4 pictureAlign">
 			<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
@@ -99,6 +105,23 @@ function projectModal(id, title, desc, date, views, author, locked) {
 			</div>
 			
 			</div>
+			
+			<div class="tab-pane fade modal-content-true" id="advisor" role="tabpanel" aria-labelledby="advisorTab">
+			<!--NEED A ROW EVERY 3 MEMBERS-->
+			<div class="row">
+			<div class="col-md-4 pictureAlign">
+			<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+			<h5>Taylor Swift</h5>
+			</div>
+			<div class="col-md-4 pictureAlign">
+			<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+			<h5>The Rock</h5>
+			</div>
+			</div>
+			
+			</div>
+			
+			<div class="tab-pane fade modal-content-true text-center" id="qrTest" role="tabpanel" href="${QRurl}" aria-labelledby="qrTab"></div>
 		</div>
 		
 		</div>
